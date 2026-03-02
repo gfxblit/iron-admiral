a4c2e5e84e23e7711a095772a537b252b25375bb
Bill Chen <3580951+gfxblit@users.noreply.github.com>
2026-03-01 20:46:57 -0800

    style: run formatter and linter

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || "/",
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.ts']
-    }
-  }
-})
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.ts"],
+    },
+  },
+});
